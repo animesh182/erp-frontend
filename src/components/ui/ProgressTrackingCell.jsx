9e2f46e5402df8d9ec165275f8b7c621c95704b7
Yush Shrestha <yushshrestha@Yushs-MacBook-Pro.local>
2024-10-07 09:57:21 +0100

    Projects dashboard added

--- src/components/ui/ProgressTrackingCell.jsx
+++ src/components/ui/ProgressTrackingCell.jsx
@@ -3,15 +3,15 @@ import React, { useState } from "react";
 import { Slider } from "@/components/ui/slider";
 import { toast } from "sonner";
 
-export const ProgressTrackingCell = ({ row }) => {
-  const [progress, setProgress] = useState(row.progress);
+export const ProgressTrackingCell = ({ row, projectName }) => {
+  const [progress, setProgress] = useState(row);
 
   const handleProgressChange = (newValue) => {
     setProgress(newValue[0]);
 
     //add api call to update the progress
-    toast.success(`Updated progress for ${row?.name} to ${newValue[0]}%`);
-    console.log(`Updated progress for ${row?.id} to ${newValue[0]}%`);
+    toast.success(`Updated progress for ${projectName} to ${newValue[0]}%`);
+    console.log(`Updated progress for ${projectName} to ${newValue[0]}%`);
   };
 
   return (
@@ -22,6 +22,7 @@ export const ProgressTrackingCell = ({ row }) => {
         max={100}
         step={10}
         className="mr-2"
+        disabled //disabled because no API
       />
       <span className="min-w-[40px] text-right">{progress}%</span>
     </div>
