57cd503308da7bd051389e52b300c7c55634fd70
Sakar-322 <sakar.droogie@gmail.com>
2024-10-18 12:29:34 +0545

    placeholder fixes | progress update api integration | minor fixes

--- src/components/MultiLineNameCell.jsx
+++ src/components/MultiLineNameCell.jsx
@@ -2,12 +2,21 @@ import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import React from "react";
 
 const MultiLineNameCell = ({ imageUrl, title, subtitle, designation }) => {
+  const getInitials = (name) => {
+    return name
+      .split(" ")
+      .map((word) => word[0])
+      .join("")
+      .toUpperCase()
+      .slice(0, 2);
+  };
+
   return (
     <div className="flex items-start">
       <Avatar>
         <AvatarImage src={imageUrl} className="object-cover" />
         <AvatarFallback>
-          <span>{title}</span>
+          {imageUrl ? <span>{title}</span> : getInitials(title)}
         </AvatarFallback>
       </Avatar>
       <div className="ml-3">
