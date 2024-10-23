c5f870197d74afd62bb6ad3952c10727875b7ded
Sankalpa-Joshi <sankalpajoshi351@gmail.com>
2024-10-23 15:21:57 +0545

    static employee login pages made and merged with crystal

--- src/app/users/dashboard/page.js
+++ src/app/users/dashboard/page.js
@@ -114,6 +114,7 @@ const UsersHome = () => {
             {tabs.map((tab) => (
               <Button
                 key={tab}
+                variant="employeePageBtn"
                 className={`px-4 py-2 text-sm font-medium rounded-lg ${
                   selectedTab === tab
                     ? "bg-blue-50 text-blue-600 border border-blue-500"
