9dd2658f664f2ea443f6839a81bd6b3199907729
Crystal <crystalstha91@gmail.com>
2024-10-28 16:04:27 +0545

    Fixes

---
+++ src/app/api/employees/getEmployeeLeaveRequest.js
@@ -0,0 +1,11 @@
+// import { apiClient } from "@/lib/utils";
+
+// export async function getEmployeeLeaveRequest(userId) {
+//     try {
+//         const response = await apiClient(
+//             `${process.env.NEXT_PUBLIC_API_URL}api/users/employee-details/${userId}`
+//         );
+//         request
+//     }
+
+// }
