8b186c34403ca126c868c525da9369219f20516a
Crystal <crystalstha91@gmail.com>
2024-10-29 17:04:08 +0545

    Merge

--- src/app/api/employees/getEmployeeLeaveRequest.js
+++ src/app/api/employees/getEmployeeLeaveRequest.js
@@ -1,29 +1,15 @@
-<<<<<<< HEAD
-// import { apiClient } from "@/lib/utils";
-
-// export async function getEmployeeLeaveRequest(userId) {
-//     try {
-//         const response = await apiClient(
-//             `${process.env.NEXT_PUBLIC_API_URL}api/users/employee-details/${userId}`
-//         );
-//         request
-//     }
-
-// }
-=======
 import { apiClient } from "@/lib/utils";
 
 export async function getEmployeeLeaveRequest(userId) {
-    try {
-      const response = await apiClient(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/leave_records/${userId}/`
-      );
-      return  response;
-    } catch (error) {
-      return {
-        status: error.status || 500,
-        message: error.message || "Failed to fetch the employee's leave request",
-      };
-    }
+  try {
+    const response = await apiClient(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/leave_records/${userId}/`
+    );
+    return response;
+  } catch (error) {
+    return {
+      status: error.status || 500,
+      message: error.message || "Failed to fetch the employee's leave request",
+    };
   }
->>>>>>> 583eaaeb20d049874a6140088f55d1dcf1eb716a
+}
